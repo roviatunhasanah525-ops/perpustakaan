ba310a47237bafd89b0510d88337b000f47ae3fe
Ovi <email_roviatunhasanah525@gmail.com>
2026-05-02 20:42:29 +0700

    menambahkan koneksi database

---
+++ CONFIG/Database.php
@@ -0,0 +1,18 @@
+<?php
+class Database {
+    private $host = "localhost";
+    private $user = "root";
+    private $pass = "";
+    private $db = "perpustakaan";
+
+    public function connect() {
+        $conn = new mysqli($this->host, $this->user, $this->pass, $this->db);
+
+        if ($conn->connect_error) {
+            die("Koneksi gagal : " . $conn->connect_error);
+        }
+
+        return $conn;
+    }
+}
+?>
